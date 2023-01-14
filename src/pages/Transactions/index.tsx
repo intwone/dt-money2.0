977ae7bdb9367d060b665e8c473cade23f61bd97
Cassio Oliveira <cassio.oliveira@pitzi.com.br>
2023-01-14 06:50:09 -0300

    feat: add Transactions pages

---
+++ src/pages/Transactions/index.tsx
@@ -0,0 +1,43 @@
+import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components/SearchForm";
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
+
+export function Transactions() {
+  return (
+    <div>
+      <Header />
+
+      <Summary />
+
+
+      <TransactionsContainer>
+        <SearchForm />
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td width="50%">
+                <PriceHighlight variant="income">
+                  R$ 12.000,00
+                </PriceHighlight>
+              </td>
+              <td width="50%">Venda</td>
+              <td width="50%">13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td width="50%">
+                <PriceHighlight variant="outcome">
+                  - R$ 12.000,00
+                </PriceHighlight>
+              </td>
+              <td width="50%">Venda</td>
+              <td width="50%">13/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
+    </div>
+  )
+}
