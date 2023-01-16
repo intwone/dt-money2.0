78da9fca045accdf8afa861a7e7733fffee4be85
Cassio Oliveira <cassio.oliveira@pitzi.com.br>
2023-01-16 07:58:26 -0300

    feat: replace fetch to axios

---
+++ src/config/envs.ts
@@ -0,0 +1,3 @@
+export const envs = {
+  api: import.meta.env.VITE_API_URL 
+}
---
+++ src/lib/axios.ts
@@ -0,0 +1,6 @@
+import axios from 'axios'
+import { envs } from '../config/envs'
+
+export const api = axios.create({
+  baseURL: envs.api
+})
